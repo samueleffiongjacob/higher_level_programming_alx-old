8b2e9ba01500f56e2927751c552902d1adeebe02
samueleffiongjacob <samueleffiong685@gmail.com>
2023-03-14 06:44:26 -0700

    2-arguments.js

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,10 +2,9 @@
 
 const len = process.argv.length;
 
-if (len < 3) {
+if (len === 3) {
   console.log('No argument');
-} else if (len === 3) {
+} else if (len < 3) {
   console.log('Argument found');
-} else {
-  console.log('Arguments found');
+} else { console.log('Arguments found');
 }
