a3e017bccffea55727ee588aea1699ce7bdb6fad
samueleffiongjacob <samueleffiong685@gmail.com>
2023-04-25 19:34:15 +0000

    webscraping for alx

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,11 +1,8 @@
 #!/usr/bin/node
 const fs = require('fs');
-const file = process.argv[2];
 
-fs.readfile(file, 'utf8', (error, data) => {
-	if (error) {
-		console.log(error);
-	} else {
-		console.log(data)
-	}
-})
+
+fs.readFile("cisfun",'utf8',function(error,content){
+	console.log(error || content);
+
+});
