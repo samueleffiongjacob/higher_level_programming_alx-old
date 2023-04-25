6f36c84ab2490f148b735d2520ba6ee23db9bd60
samueleffiongjacob <samueleffiong685@gmail.com>
2023-04-25 19:41:18 +0100

    javascript webscrading script

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,5 +1,11 @@
 #!/usr/bin/node
 const fs = require('fs');
-fs.readFile(process.argv[2], 'utf8', function (error, content) {
-  console.log(error || content);
-});
+const file = process.argv[2];
+
+fs.readfile(file, 'utf8', (error, data) => {
+	if (error) {
+		console.log(error);
+	} else {
+		console.log(data)
+	}
+})
