660d6e8534871dc646e05df4d51bc515665415e3
samueleffiongjacob <samueleffiong685@gmail.com>
2023-05-24 08:58:20 -0700

    fixed 0x01 python

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python3
-charend = ", "
-for i in range(0, 100):
-    samuel = i % 10
-    samuel1 = i / 10
-    if i < 10 and samuel < samuel1:
-        print("{}{}".format(0, i), end=charend)
-    elif samuel < samuel1:
-        if i == 89:
-            charend = "\n"
-        print(i, end=charend)
+#!/usr/bin/python3
+for i in range(100):
+    if int(i / 10) != i % 10 and int(i / 10) < i % 10:
+        print("{}{}".format(int(i / 10), i % 10), end="")
+        if (i != 89):
+            print(", ", end="")
+print("")
