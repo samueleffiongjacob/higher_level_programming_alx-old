94761adc4eec3502c5227a827b93280c06e7ddce
samueleffiongjacob <samueleffiong685@gmail.com>
2023-05-24 07:19:00 -0700

    fixed for 0x00-python

--- 0x00-python-hello_world/5-print_string.py
+++ 0x00-python-hello_world/5-print_string.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
-"" " str = "Holberton School"
-print("{}".format(str) * 3)
-print(str[:9])"
+#str = "Holberton School"
+#print("{}".format(str) * 3)
+#print(str[:9])"
 
  print(3 * str)
  print(str[:9])
