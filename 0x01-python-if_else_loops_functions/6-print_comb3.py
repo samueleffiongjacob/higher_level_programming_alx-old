db99a3337f589470cdc89ae76ff1625616a6f59e
samueleffiongjacob <samueleffiong685@gmail.com>
2023-05-24 08:54:00 -0700

    fixed for python 0x01

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 charend = ", "
 for i in range(0, 100):
-    digit2 = i % 10
-    digit1 = i / 10
-    if i < 10 and digit1 < digit2:
+    samuel = i % 10
+    samuel1 = i / 10
+    if i < 10 and samuel < samuel1:
         print("{}{}".format(0, i), end=charend)
-    elif digit1 < digit2:
+    elif samuel < samuel1:
         if i == 89:
             charend = "\n"
         print(i, end=charend)
